781c5404e2d4da1c88c4a314d0c497791f6004f7
Joao Paulo Carnellossi dos Santos <joao951951>
2023-04-24 14:46:47 -0300

    seeder edited

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(FornecedorSeeder::class);
+        $this->call(SiteContatoSeeder::class);
+        $this->call(MotivoContatoSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
